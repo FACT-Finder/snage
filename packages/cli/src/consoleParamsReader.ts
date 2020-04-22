f2f62850b3552ba76cd009c05a2d9ab80efc8c08
matthias.ott <matthias.ott@omikron.net>
2020-04-22 09:42:57 +0200

    Add --noWizard mode

--- packages/cli/src/consoleParamsReader.ts
+++ packages/cli/src/consoleParamsReader.ts
@@ -3,12 +3,14 @@ import {Config, Field} from "../../shared/type";
 import {askUserForFieldValue} from "./consoleWizzard";
 import {isValidDate} from "./validators";
 import {expectNever} from "../../server/src/util/util";
-import {Either, left, right} from "fp-ts/lib/Either";
+import {Either, isLeft, left, right} from "fp-ts/lib/Either";
+
+const NO_WIZARD_LABEL:string = "noWizard";
 
 const addType = (field: Field, yargs: yargs.Argv) => {
     if (field.list) {
         yargs.array(field.name);
-        return true;
+        return;
     }
     switch (field.type) {
         case "semver": // intentional fallthrough since semver isn't supported by yargs
@@ -16,30 +18,35 @@ const addType = (field: Field, yargs: yargs.Argv) => {
         case "ffversion": // intentional fallthrough since ffversion isn't supported by yargs
         case "string": {
             yargs.string(field.name);
-            return true;
+            return;
         }
         case "boolean": {
             yargs.boolean(field.name);
-            return true;
+            return;
         }
         case "number": {
             yargs.number(field.name);
-            return true;
+            return;
         }
         default: {
-            return expectNever(field.type)
+            expectNever(field.type)
         }
     }
 };
 
 const addDescription = (fields: Field[], yargs: yargs.Argv) => {
     let description = {};
+    description[NO_WIZARD_LABEL] = "Prevents the wizard from starting when fields aren't set. Options are: \n" +
+        "t - fills all missing required fields with the needed data type as placeholder\n" +
+        "to - fills all missing required and optional fields with the needed data type as placeholder\n" +
+        "if no option is provided, the fields will simply be missing, but must be added per hand to obtain a valid change log";
     fields.forEach(field => description[field.name] = field.description ? field.description : '');
     yargs.describe(description);
 };
 
 const addAlias = (fields: Field[], yargs: yargs.Argv) => {
     let alias = {};
+    alias[NO_WIZARD_LABEL] = "nw";
     fields.filter(field => field.alias).map(field => alias[field.name] = field.alias);
     yargs.alias(alias);
 };
@@ -55,6 +62,7 @@ const addAlias = (fields: Field[], yargs: yargs.Argv) => {
  */
 export const parseLogParameters = async (nodeArgV: string[], config: Config): Promise<Either<string, {}>> => {
     const builder = yargs(nodeArgV);
+    builder.string(NO_WIZARD_LABEL);
     config.fields.forEach(field => addType(field, builder));
     addDescription(config.fields, builder);
     addAlias(config.fields, builder);
@@ -72,11 +80,42 @@ const buildLogParameters = async (fields: Field[], consoleArguments: {}, support
             }
             returnValues[field.name] = consoleArguments[field.name];
         } else {
-            let fieldValue = await askUserForFieldValue(field, supportedDateFormat);
-            if (fieldValue != null) {
-                returnValues[field.name] = fieldValue;
+            const result = await handleMissingValue(field, consoleArguments, supportedDateFormat, returnValues);
+            if(isLeft(result)){
+                return left(result.left);
             }
         }
     }
     return right(returnValues);
+};
+
+const handleMissingValue = (async (field: Field, consoleArguments: {}, supportedDateFormat: string, returnValues: {}): Promise<Either<string, true>> => {
+    if (consoleArguments[NO_WIZARD_LABEL] == null) {
+        let fieldValue = await askUserForFieldValue(field, supportedDateFormat);
+        if (fieldValue != null) {
+            returnValues[field.name] = fieldValue;
+        }
+    } else if (consoleArguments[NO_WIZARD_LABEL] == 'to' || (consoleArguments[NO_WIZARD_LABEL] == 't' && !field.optional)) {
+        fillEmptyFieldWithDataType(field, supportedDateFormat, returnValues);
+    } else if (consoleArguments[NO_WIZARD_LABEL] != null && !(consoleArguments[NO_WIZARD_LABEL] == 'to' || consoleArguments[NO_WIZARD_LABEL] == 't')){
+        return left('Invalid usage of --' + NO_WIZARD_LABEL + ": " + consoleArguments[NO_WIZARD_LABEL] + ". Check --help for more info.");
+    }
+    return right(true);
+});
+
+const fillEmptyFieldWithDataType = (field: Field, supportedDateFormat: string, returnValues: {}) => {
+    if(field.enum) {
+        returnValues[field.name] = getEnumString(field);
+    } else {
+        returnValues[field.name] = field.type == "date"? supportedDateFormat : field.type;
+    }
+    if(field.list) {
+        returnValues[field.name] = [returnValues[field.name]];
+    }
+};
+
+const getEnumString = (field: Field): string => {
+    let enumString: string = "";
+    field.enum?.forEach(entry => enumString = enumString + entry + ' | ');
+    return enumString.substr(0, enumString.length -3);
 };
