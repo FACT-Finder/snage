4d054111dab1c5c3268fc1c54257ba152cfe353c
Jannis Mattheis <jannis.mattheis@omikron.net>
2020-04-24 09:45:17 +0200

    Use expansion panel for ui

--- packages/ui/src/App.tsx
+++ packages/ui/src/App.tsx
@@ -1,47 +1,69 @@
 import React from 'react';
 import './App.css';
-import {Note} from "../../shared/type";
+import {Note} from '../../shared/type';
 import Chip from '@material-ui/core/Chip';
-import ReactMarkdown from "react-markdown";
+import ReactMarkdown from 'react-markdown';
 import axios from 'axios';
+import {Typography, ExpansionPanel, ExpansionPanelSummary, ExpansionPanelDetails, makeStyles} from '@material-ui/core';
 
 const App: React.FC = () => {
     const [entries, setEntries] = React.useState<Note[]>([]);
 
-
     return (
         <div className="App">
-            <h1 style={{textAlign: 'center'}}>FACT-Finder Changelog</h1>
-            <Search setEntries={setEntries}/>
+            <h1 style={{textAlign: 'center'}}>Changelog</h1>
+            <Search setEntries={setEntries} />
             <div>
-                {entries.map(entry => <Entry key={entry.id} entry={entry}/>)}
+                {entries.map((entry) => (
+                    <Entry key={entry.__id} entry={entry} />
+                ))}
             </div>
         </div>
     );
-}
+};
 
-const Search: React.FC<{ setEntries: (e: Note[]) => void }> = ({setEntries}) => {
+const Search: React.FC<{setEntries: (e: Note[]) => void}> = ({setEntries}) => {
     const [query, setQuery] = React.useState('');
 
     React.useEffect(() => {
-        axios.get(`/note?query=${query}`).then(resp => {
+        axios.get(`/note?query=${query}`).then((resp) => {
             setEntries(resp.data);
             return;
         });
     }, [query, setEntries]);
 
-    return <div style={{textAlign: 'center', padding: 30}}><input type="text" style={{width: 500}} value={query} onChange={(e) => setQuery(e.target.value)}/>
-    </div>
+    return (
+        <div style={{textAlign: 'center', padding: 30}}>
+            <input type="text" style={{width: 500}} value={query} onChange={(e) => setQuery(e.target.value)} />
+        </div>
+    );
 };
 
-const Entry = React.memo(({entry: {name, content, ...other}}: { entry: Note }) => {
-    return <div style={{}}>
-        <div>
-            {Object.entries(other).map(([key, value]) => <Chip key={key} style={{marginRight: 10}} label={key + ': ' + value}/>)}
-        </div>
-        <ReactMarkdown source={content}/>
-        <hr/>
-    </div>;
+const Entry = React.memo(({entry: {__content, __summary, __id, ...other}}: {entry: Note}) => {
+    const classes = useStyles();
+    return (
+        <ExpansionPanel>
+            <ExpansionPanelSummary>
+                <div>
+                    <Typography variant="h5" component="div" className={classes.header}>
+                        <ReactMarkdown source={__summary} />
+                    </Typography>
+                    {Object.entries(other).map(([key, value]) => (
+                        <Chip key={key} style={{marginRight: 10}} label={key + ': ' + value} />
+                    ))}
+                </div>
+            </ExpansionPanelSummary>
+            <ExpansionPanelDetails>
+                <div>{__content === '' ? '-- no content --' : <ReactMarkdown source={__content} />}</div>
+            </ExpansionPanelDetails>
+        </ExpansionPanel>
+    );
+});
+
+const useStyles = makeStyles({
+    header: {
+        '& > p': {margin: '0'},
+    },
 });
 
 export default App;
