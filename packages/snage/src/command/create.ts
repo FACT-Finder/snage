09ffdebafb0ae9ec4557dbc0de51c9ab610e4f62
Jannis Mattheis <jannis.mattheis@omikron.net>
2020-04-24 09:05:03 +0200

    Fix help of `snage create`

--- packages/snage/src/command/create.ts
+++ packages/snage/src/command/create.ts
@@ -1,5 +1,5 @@
 import yargs from 'yargs';
-import {DefaultCli} from './common';
+import {DefaultCli, DefaultSnageConfig, ConfigParameterName, EnvPrefix} from './common';
 import {validateFileNameSchema} from '../create/validators';
 import {isLeft} from 'fp-ts/lib/Either';
 import {addToYargs, buildLogParameters} from '../create/consoleParamsReader';
@@ -7,12 +7,22 @@ import {generateChangeLogFile} from '../create/changelogFileWriter';
 import {extractFieldsFromFileName} from '../create/genChangelog';
 import {loadConfig} from '../config/load';
 
+// We cannot use yargs.argv because it will abort the building of the parameters.
+// Sadly we need the config from the parameters to build the arguments.
+// Thus, we manually parse the config parameter.
+const uglyGetConfig = (): string => {
+    const cfgIdx = process.argv.findIndex((arg) => arg === `--${ConfigParameterName}`);
+    if (cfgIdx !== -1 && process.argv.length > cfgIdx + 1) {
+        return process.argv[cfgIdx + 1];
+    }
+    return process.env[`${EnvPrefix}_${ConfigParameterName.toUpperCase()}`] ?? DefaultSnageConfig;
+};
+
 export const create: yargs.CommandModule<DefaultCli, DefaultCli> = {
     command: 'create',
     describe: 'Create a change log file.',
     builder: (y) => {
-        const {config: configFile} = y.argv;
-        const config = loadConfig(configFile);
+        const config = loadConfig(uglyGetConfig());
         const fileNames: string[] = extractFieldsFromFileName(config);
         const fileNameIsValid = validateFileNameSchema(config, fileNames);
         if (isLeft(fileNameIsValid)) {
