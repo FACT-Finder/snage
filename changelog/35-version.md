3daf8b11683644ee200c5850aae027eb1e4eb68f
Jannis Mattheis <jannis.mattheis@omikron.net>
2020-04-24 12:24:14 +0200

    Fix `snage --version`

---
+++ changelog/35-version.md
@@ -0,0 +1,9 @@
+---
+issue: '#35'
+date: '2020-04-24'
+audience: user
+type: fix
+components: [cli]
+---
+
+# Return correct version number on `snage --version`
--- packages/snage/webpack.prod.js
+++ packages/snage/webpack.prod.js
@@ -1,3 +1,8 @@
 const common = require('./webpack.common.js');
+const webpack = require('webpack');
 
-module.exports = {...common, mode: 'production'};
+module.exports = {
+    ...common,
+    plugins: [...common.plugins, new webpack.DefinePlugin({'process.env.BUILD_VERSION': `'${process.env.BUILD_VERSION || 'unknown'}'`})],
+    mode: 'production',
+};
