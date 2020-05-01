7b194798c095c49538dc19f2cc562aaf88094740
Thomas von Rosenberg <tvr@omikron.net>
2020-05-01 15:29:10 +0200

    Make note parsing async

--- packages/snage/src/command/lint.ts
+++ packages/snage/src/command/lint.ts
@@ -1,18 +1,19 @@
 import yargs from 'yargs';
 import {loadConfig, resolveChangelogDirectory} from '../config/load';
 import {errorToString, parseNotes} from '../note/parser';
-import * as E from 'fp-ts/lib/Either';
+import * as TE from 'fp-ts/lib/TaskEither';
+import * as T from 'fp-ts/lib/Task';
 import {DefaultCli, printAndExit} from './common';
 import {pipe} from 'fp-ts/lib/pipeable';
 
 export const lint: yargs.CommandModule<DefaultCli, DefaultCli> = {
     command: 'lint',
     describe: 'Lint all change log files.',
-    handler: ({config: configFilePath}) => {
-        pipe(
-            loadConfig(configFilePath),
-            E.chain((config) => pipe(parseNotes(config, resolveChangelogDirectory(config, configFilePath)), E.mapLeft(errorToString))),
-            E.fold(printAndExit, () => console.log('All good :D'))
-        );
+    handler: async ({config: configFilePath}) => {
+        return pipe(
+            TE.fromEither(loadConfig(configFilePath)),
+            TE.chain((config) => pipe(parseNotes(config, resolveChangelogDirectory(config, configFilePath)), TE.mapLeft(errorToString))),
+            TE.fold(T.fromIOK(printAndExit), () => T.fromIO(() => console.log('All good :D')))
+        )();
     },
 };
