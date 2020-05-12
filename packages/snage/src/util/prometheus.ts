b1b8b9ef0beca38a40d5622b80a3d7365ae6a378
Thomas von Rosenberg <tvr@omikron.net>
2020-05-12 15:24:50 +0200

    Add metrics endpoint

---
+++ packages/snage/src/util/prometheus.ts
@@ -0,0 +1,15 @@
+import {Gauge, Histogram} from 'prom-client';
+
+export const totalNotes = new Gauge({help: 'number of notes', name: 'snage_notes_total'});
+
+const requestHistogram = new Histogram({
+    help: 'request duration buckets in seconds',
+    name: 'snage_request_duration',
+    labelNames: ['path', 'status'],
+});
+
+export const startRequestTimer = (path: string): ((status: number) => void) => {
+    const endTimer = requestHistogram.startTimer({path});
+
+    return (status: number): void => endTimer({status});
+};
