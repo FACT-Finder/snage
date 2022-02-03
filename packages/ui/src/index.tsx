65f7453768d22860a594e1425c403def2d82ef47
Jannis Mattheis <jannis.mattheis@omikron.net>
2022-02-03 11:51:30 +0100

    Always use light mode markdown

--- packages/ui/src/index.tsx
+++ packages/ui/src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import 'github-markdown-css';
+import 'github-markdown-css/github-markdown-light.css';
 import './index.css';
 import App from './App';
 
